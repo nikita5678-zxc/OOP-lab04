6356bc04796db1fdb0434f154affbc6ca3c75896
nikita5678-zxc <nikita_deybert1111@mail.ru>
2025-11-13 22:19:23 +0300

    inital commit

--- main.cpp
+++ main.cpp
@@ -10,17 +10,31 @@ int main() {
     Array<std::shared_ptr<Figure<double>>> figures;
 
     auto tri = std::make_shared<Triangle<double>>(
-        Point<double>(0, 0), Point<double>(1, 0)
+        Point<double>(0, 0),
+        Point<double>(1, 0),
+        Point<double>(0, 1)
     );
     figures.add(std::move(tri));
 
     auto hex = std::make_shared<Hexagon<double>>(
-        Point<double>(3, 0), Point<double>(4, 0)
+        Point<double>(2, 0),
+        Point<double>(3, 0),
+        Point<double>(4, 1),
+        Point<double>(3, 2),
+        Point<double>(2, 2),
+        Point<double>(1, 1)
     );
     figures.add(std::move(hex));
 
     auto oct = std::make_shared<Octagon<double>>(
-        Point<double>(0, 3), Point<double>(1, 3)
+        Point<double>(0, 3),
+        Point<double>(1, 3),
+        Point<double>(2, 4),
+        Point<double>(2, 5),
+        Point<double>(1, 6),
+        Point<double>(0, 6),
+        Point<double>(-1, 5),
+        Point<double>(-1, 4)
     );
     figures.add(std::move(oct));
 
@@ -44,13 +58,17 @@ int main() {
 
     Array<std::shared_ptr<Octagon<int>>> octagons;
     octagons.add(std::make_shared<Octagon<int>>(
-        Point<int>(0, 0), Point<int>(2, 0)
-    ));
-    octagons.add(std::make_shared<Octagon<int>>(
-        Point<int>(5, 5), Point<int>(7, 5)
+        Point<int>(0, 0),
+        Point<int>(2, 0),
+        Point<int>(3, 1),
+        Point<int>(2, 2),
+        Point<int>(0, 2),
+        Point<int>(-1, 2),
+        Point<int>(-2, 1),
+        Point<int>(-1, 0)
     ));
 
-    std::cout << "Integer octagons:\n";
+    std::cout << "Integer octagon:\n";
     for (size_t i = 0; i < octagons.getSize(); ++i) {
         std::cout << *octagons[i] << "\n";
         std::cout << "Area: " << static_cast<double>(*octagons[i]) << "\n";
